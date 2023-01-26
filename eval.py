126901e41a99f6fd4c99d653f190be751fcac64e
sahandp2012p <spourjavad2012@gmail.com>
2023-01-26 14:13:59 +0330

    Created evaluation dataframe

--- eval.py
+++ eval.py
@@ -1,5 +1,26 @@
 # Evaluation model
 
 from random_board_generator import generate_random_board
+import chess.engine
+import pandas as pd
 
-positions = [generate_random_board() for _ in range(2000000)]
+positions = [generate_random_board(max_depth=50) for _ in range(200)]
+evals = []
+
+# Loading stockfish
+
+engine = chess.engine.SimpleEngine.popen_uci("./stockfish/15/stockfish-windows-2022-x86-64-avx2.exe")
+
+for position in positions:
+    evals.append(engine.analyse(position, chess.engine.Limit(depth=18))['score'].white().score())
+    print(engine.analyse(position, chess.engine.Limit(depth=18))['score'].white().score())
+
+
+# Creating the dataframe to train the model
+
+df = pd.DataFrame(columns=['position', 'eval'])
+
+df['position'] = positions
+df['eval'] = evals
+
+print(df.head())
